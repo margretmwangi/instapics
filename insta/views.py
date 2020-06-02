64fe4aabf598a987cc473b7b4701036cb0e56d71
margretmwangi <margaret13mwangi@gmail.com>
2020-06-02 22:28:33 +0300

    addeded registration

--- insta/views.py
+++ insta/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import post
+from django.contrib import messages
+from.forms import UserRegisterForm
 from django.views.generic import(
     ListView,
 
@@ -10,4 +12,16 @@ class PostListView(ListView):
     queryset = post.objects.all()
     context_object_name = 'posts'
 
+def registration(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request,f'Account created for {username}')
+            return redirect('/')
+    else :
+
+        form = UserRegisterForm()
+    return render(request,'registration/register.html' , {'form':form})
 
--- instaclone/settings.py
+++ instaclone/settings.py
@@ -121,6 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 LOGIN_REDIRECT_URL = '/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 MEDIA_URL = '/media/'
