94ec0a58e8e107f54421635cb26e68cee5747863
margretmwangi <margaret13mwangi@gmail.com>
2020-06-07 19:11:50 +0300

    updated STATIC_ROOT

--- instaclone/settings.py
+++ instaclone/settings.py
@@ -120,7 +120,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'media')
 STATIC_URL = '/static/'
+
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 LOGIN_URL = 'home'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
